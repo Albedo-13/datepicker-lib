42dca144ca89e9f64cb70d470f4781932b2fd876
PavelProkopenya <prokopenya.personal@gmail.com>
2024-04-28 14:45:53 +0300

    refactor: rename v to variables

--- src/components/date-picker/styles.ts
+++ src/components/date-picker/styles.ts
@@ -1,7 +1,7 @@
 import { styled } from "styled-components";
 
-import { variables as v } from "@/styles/_variables";
+import { variables } from "@/styles/_variables";
 
 export const Wrapper = styled.div`
-  width: ${v.spacing.s350};
+  width: ${variables.spacing.s350};
 `;
